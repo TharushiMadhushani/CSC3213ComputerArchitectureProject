ed41173116fa06dd5786b7e1cba1703823f6c8e9
TharushiMadhushani <139611254+TharushiMadhushani@users.noreply.github.com>
2025-03-22 07:09:40 +0000

    fundamental assembly programs that introduce basic concepts

---
+++ Day1/d1_1.asm
@@ -0,0 +1,23 @@
+.model small
+.stack 100h
+
+.data
+m db 5
+
+.code
+start:
+
+mov ax,@data
+mov ds,ax
+
+mov dl,m ;d1=5
+add dl,48 ;48+5=53
+mov ah,02h
+int 21h
+
+mov ax,4c00h
+int 21h
+
+
+end start
+
